3fa3beaf551f7edd564ba3ef27dc1e3dc1f658c2
Kathleen Garrity <garrityk@bc.edu>
2019-11-17 23:09:56 +0000

    adding ratelimiter to handle rate limit exception

--- data_collection.py
+++ data_collection.py
@@ -1,8 +1,19 @@
 from github import Github
 
+from ratelimiter import RateLimiter
+import time
+
+def limited(until):
+    duration = int(round(until - time.time()))
+    print("Rate limited, sleeping for {:d} seconds".format(duration))
+
+rate_limiter = RateLimiter(max_calls=2, period=3, callback=limited)
+
 g = Github("3e0d6fcd7bfcd3420bc62d9b5f717e99ddd820e8")
 
-repositories = g.search_repositories(query='language: python')
+def collect_python_repos():
+    repositories = list(g.search_repositories(query='language: python', sort='stars'))
+    return repositories[0]
 
-for repo in repositories:
-    print(repo)
+
+print(collect_python_repos())
