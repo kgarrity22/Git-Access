1b09784847a6c5dd46b6244c49598b8d540f3dc6
Kathleen Garrity <garrityk@bc.edu>
2019-11-24 18:11:39 +0000

    intial work with geopy to handle locations

--- data_collection.py
+++ data_collection.py
@@ -13,7 +13,26 @@ g = Github("3e0d6fcd7bfcd3420bc62d9b5f717e99ddd820e8")
 
 def collect_python_repos():
     repositories = list(g.search_repositories(query='language: python', sort='stars'))
-    return repositories[0]
+    return repositories[:10]
+
+# print(collect_python_repos())
+
+import geopy.geocoders
+from geopy.geocoders import Nominatim
+
+####geopy.geocoders.options.defult_user_agent = 'my_app/1'
+##geopy.geocoders.options.default_timeout = 7
+##geolocator = Nominatim(user_agent="https://github.com/kgarrity22")
+##location = geolocator.geocode("Boston, MA")
+##print((location.latitude, location.longitude))
+import ssl
+import geopy.geocoders
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+geopy.geocoders.options.default_ssl_context = ctx
 
 
-print(collect_python_repos())
+geolocator = Nominatim()
+location = geolocator.geocode("175 5th Avenue NYC")
+print(location.address)
