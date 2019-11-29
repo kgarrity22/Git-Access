5f3100bf457a7ae8fab8a81d63483a63ae11e8d1
Kathleen Garrity <garrityk@bc.edu>
2019-11-29 17:45:54 +0000

    added code to create csv with user cmmt number and location

--- data_collection.py
+++ data_collection.py
@@ -4,79 +4,134 @@ import time
 import geopy.geocoders
 from geopy.geocoders import Nominatim
 import operator
-
-
-# def limited(until):
-#     duration = int(round(until - time.time()))
-#     print("Rate limited, sleeping for {:d} seconds".format(duration))
-#
-# rate_limiter = RateLimiter(max_calls=2, period=3, callback=limited)
+import ssl
 
 g = Github("3e0d6fcd7bfcd3420bc62d9b5f717e99ddd820e8")
 
-# create a dictionary to collect the users and count their number of commits
+# get the top 10 most starred python repos
+repositories = g.search_repositories(query='language: python', sort='stars', order='desc')
+top_10 = list(repositories[:10])
+
+
+# collect the number of commits each user has made to a repo and return data as a dictionary
+def get_commit_info(dictionary, repo):
+    # get all the commits made to that repo
+    commits = repo.get_commits()
+
+    # for each commit, find the user
+    # update the count of a user's commit number if they are already in the dictionary
+    # otherwise, add the user to the dictionary (with a commit count of 1)
+    for commit in commits:
+        user=commit.committer
+        if str(type(user)) == "<class 'NoneType'>":
+            dictionary[user]=0
+        elif user in d:
+            dictionary[user]+=1
+        else:
+            dictionary[user]=1
+
+    return dictionary
+
+# combine two dictionaries (used to add a user's total commits across repos)
+# returns a single dictionary with updated user commit #'s
+def combine_dicts(d1, d2):
+    for item in d2:
+        if item in d1:
+            d1[item]= d1[item] + d2[item]
+        else:
+            d1[item]=1
+
+    return d1
+
+
 d = dict()
+# create a dictionary for the commits of each repo
+# sleep in between to prevent rate limiting
+d1 = get_commit_info(d, top_10[0])
+time.sleep(1800)
 
-def collect_python_repos():
+newdict2 = dict()
+d2 = get_commit_info(newdict2, top_10[1])
+time.sleep(1800)
 
-    # collect all the python repos in order of highest number of stars
-    repositories = g.search_repositories(query='language: python', sort='stars', order='desc')
+newdict3 = dict()
+d3 = get_commit_info(newdict3, top_10[2])
+time.sleep(1800)
 
-    # looking at the 10 most starred python repositories
-    for repo in repositories[:10]:
-        # sleep for 15 minutes before each iteration to avoid RateLimit error
-        time.sleep(900)
-        # get all the commits made to that repo
-        commits = repo.get_commits()
-        # for each commit, find the user
-        # update the count of a user's commit number if they are already in the dictionary
-        # otherwise, add the user to the dictionary (with a commit count of 1)
-        for commit in commits:
-            user=commit.committer
-            if user in d:
-                d[user]+=1
-            else:
-                d[user]=1
+newdict4 = dict()
+d4 = get_commit_info(newdict4, top_10[3])
+time.sleep(1800)
 
-    # create a list of tuples (user, commit_num) that ordered by commit number in descending order
-    sorted_by_commit = sorted(d.items(), key=operator.itemgetter(1), reverse=True)
+newdict5 = dict()
+d5 = get_commit_info(newdict5, top_10[4])
+time.sleep(1800)
 
-    # list of tuples containing the usernames and number of commits of the top 150 contributors
+newdict6 = dict()
+d6 = get_commit_info(newdict6, top_10[5])
+time.sleep(1800)
+
+newdict7 = dict()
+d7 = get_commit_info(newdict7, top_10[6])
+time.sleep(1800)
+
+newdict8 = dict()
+d8 = get_commit_info(newdict8, top_10[7])
+time.sleep(1800)
+
+newdict9 = dict()
+d2 = get_commit_info(newdict9, top_10[8])
+time.sleep(1800)
+
+newdict10 = dict()
+d10 = get_commit_info(newdict10, top_10[9])
+time.sleep(1800)
+
+d12 = combine_dicts(d1, d2)
+d34 = combine_dicts(d3, d4)
+d56 = combine_dicts(d5, d6)
+d78 = combine_dicts(d7, d8)
+d910 = combine_dicts(d9, d10)
+d14 = combine_dicts(d12, d34)
+d58 = combine_dicts(d56, d78)
+most = combine_dicts(d14, d58)
+total = combine_dicts(most, d910)
+
+# create a list of tuples (user, commit_num) ordered by commit number in descending order
+sorted_by_commit = sorted(total.items(), key=operator.itemgetter(1), reverse=True)
+
+# list of tuples containing the usernames and number of commits of the top 150 contributors
+top_50_committers = sorted_by_commit[:150]
 
 
+# # testing writing to a csv file to make sure data collection is working
+# for item in top_150_committers:
+#     row = [item[0], item[1]]
+#     with open('name.csv', 'a', encoding='utf8') as csvFile:
+#         writer = csv.writer(csvFile, quoting=csv.QUOTE_ALL)
+#         writer.writerow(row)
 
 
-
-
-
-
-
-
-
-####geopy.geocoders.options.defult_user_agent = 'my_app/1'
-##geopy.geocoders.options.default_timeout = 7
-##geolocator = Nominatim(user_agent="https://github.com/kgarrity22")
-##location = geolocator.geocode("Boston, MA")
-##print((location.latitude, location.longitude))
-# import ssl
-# import geopy.geocoders
-# ctx = ssl.create_default_context()
-# ctx.check_hostname = False
-# ctx.verify_mode = ssl.CERT_NONE
-# geopy.geocoders.options.default_ssl_context = ctx
-
+# handling SSL Certificate error
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+geopy.geocoders.options.default_ssl_context = ctx
 
 geolocator = Nominatim()
-location = geolocator.geocode("175 5th Avenue NYC")
-print(location.address)
 
-# code to get a user's location
-commit = commits[0]
-
-user = commit.committer
-print(user.location)
-
-# convert a python dictionary into a json
-import json
-d = dict()
-j = json.dumps(d)
+# write the location data to a csv 
+for person in top_150_committers:
+    user = person[0]
+    if user.location == None:
+        print('x')
+    elif str(type(user.location)) == "<class 'NoneType'>":
+        print('x')
+    else:
+        location = geolocator.geocode(user.location)
+        if location==None:
+            print('x')
+        else:
+            info = [location.longitude, location.latitude, user.location, user.name, person[1], user]
+            with open('locationdata.csv', 'a', encoding='utf8') as csvFile:
+                writer = csv.writer(csvFile, quoting=csv.QUOTE_ALL)
+                writer.writerow(info)
